035184803ce550a29ebbc9bb3571cc1905aec706
Patrik Falk <patrik@patrikfalk.com>
2014-11-27 18:53:58 +0100

    Added git-setup.sh

---
+++ git-setup.sh
@@ -0,0 +1,23 @@
+git config --global user.name "Patrik Falk"
+git config --global user.email "<patrik@patrikfalk.com>"
+
+# Git autocorrect on
+git config --global help.autocorrect 1
+
+# Push only the current branch
+git config --global push.default current
+
+# Print a graph of the log and branches
+git config --global alias.graph "log --oneline --graph --decorate --all"
+
+# Easier to read logs
+git config --global alias.l "log --pretty=format:'%cd  %H  %s (%cn)' --date=short"
+
+# Show a shorter version of status
+git config --global alias.stat "status -sb"
+
+# Diff with just the names only
+git config --global alias.d "diff --name-only"
+
+# Show the log in reverse (start of the branch)
+git config --global alias.log-rev "log --oneline --reverse"
